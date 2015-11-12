c89744dbabd57958012ca7a64f7e0f704a56c39e
Jennifer Garner <jennifergarner@mail.fresnostate.edu>
2015-11-11 18:53:48 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	r := httprouter.New()
+	http.Handle("/", r)
+	r.GET("/", index)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public/"))))
+
+	tpl = template.New("roottemplate")
+	tpl = template.Must(tpl.ParseGlob("templates/*.html"))
+}
+
+func index(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	tpl.ExecuteTemplate(res, "index.html", nil)
+}
