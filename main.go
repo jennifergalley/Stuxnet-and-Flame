300a40c95f38a9227b5b5d7523918962af594b57
Jennifer Garner <jennifergarner@mail.fresnostate.edu>
2015-11-11 20:40:41 -0800

    initial site

--- main.go
+++ main.go
@@ -12,12 +12,18 @@ func init() {
 	r := httprouter.New()
 	http.Handle("/", r)
 	r.GET("/", index)
+	r.GET("/:page", serve)
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public/"))))
 
 	tpl = template.New("roottemplate")
 	tpl = template.Must(tpl.ParseGlob("templates/*.html"))
 }
 
-func index(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+func index(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	tpl.ExecuteTemplate(res, "index.html", nil)
+}
+
+func serve(res http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	page := ps.ByName("page")
+	tpl.ExecuteTemplate(res, page + ".html", nil)
 }
